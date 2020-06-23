270d2106b45748994030cece5c23331e44a7ca8b
JTBhuvan <jairaj.tup@gmail.com>
2020-06-23 19:50:53 +0530

    define functions

---
+++ test.py
@@ -0,0 +1,11 @@
+introString=input('enter string')
+characterCount=0
+wordCount=1
+for i in introString:
+        characterCount=characterCount+1
+        if(i==' 'hv ):
+            wordCount=wordCount+1
+print(wordCount)
+print(characterCount)
+
+
