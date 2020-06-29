b0893079151bfbf5be9be2b5a2f8ba78413a5814
JTBhuvan <jairaj.tup@gmail.com>
2020-06-29 19:56:00 +0530

    test

---
+++ car.py
@@ -0,0 +1,16 @@
+class Car(object):
+    def __init__(self,model,color,company,speedLimit):
+        self.color=color
+        self.company=company
+        self.model=model
+        self.speedLimit=speedLimit
+    def start(self):
+        print("started")
+    def stop(self):
+        print("stop")    
+    def accelerate(self):
+        print("accelerating")
+    def changeGear(self,gearType):
+        print("gearChanged")
+audi=Car("a6","red","audi",80)
+print(audi.stop)
